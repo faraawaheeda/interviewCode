39c822c37cc957be4f72d9c6d83b31343e281e03
Farah Waheeda <76742944+faraawaheeda@users.noreply.github.com>
2024-10-01 23:36:09 +0800

    Delete

--- drop down filter code.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ route('filter.students') }}" method="GET">
-    <select name="class_id" onchange="this.form.submit()">
-        @foreach ($classes as $class)
-            <option value="{{ $class->id }}">{{ $class->name }}</option>
-        @endforeach
-    </select>
-</form>
